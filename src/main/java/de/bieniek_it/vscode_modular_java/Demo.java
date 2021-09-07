4287e92c385c05c80043fcbf868a6ee0dcbfe6f0
Raik Bieniek <raik@voidnode.de>
2021-09-07 19:48:49 +0200

    Demonstrates a bug in vscode-java-debug that launching apps that require both, modulepath and classpath is not working with shortenCommandLine="argfile".

---
+++ src/main/java/de/bieniek_it/vscode_modular_java/Demo.java
@@ -0,0 +1,13 @@
+package de.bieniek_it.vscode_modular_java;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Demo {
+
+    private static final Logger LOG = LogManager.getLogger(Demo.class);
+
+    public static void main(String[] args) {
+        LOG.info("Hello World!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module de.bieniek_it.vscode_modular_java {
+    requires org.apache.logging.log4j; 
+}
